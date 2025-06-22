a64ca46a5c96c15929a21c201dc2c477fc84014f
AWPSPORT <maksimilian.telezhnikov@gmail.com>
2025-06-23 01:25:01 +0300

    Added fourm and animations

--- Script.js
+++ Script.js
@@ -4,3 +4,14 @@ button.addEventListener('click', () => {
   button.classList.toggle('active')
   button.setAttribute('aria-expended', button.classList.contains('active') ? 'true': 'false')
 });
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.classList.add('hovered');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.classList.remove('hovered');
+  });
+});
